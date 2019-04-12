0ce63c91e31b3e7791f6d8eb4644f3c6b92efc5b
xavvvier <xavvvier@users.noreply.github.com>
2019-04-12 15:05:27 -0300

    Include starting screen

--- game.js
+++ game.js
@@ -11,6 +11,7 @@ var config = {
       }
    },
    scene: [
+      SceneMain,
       SceneOne,
       SceneTwo,
       SceneGameWin,
---
+++ sceneMain.js
@@ -0,0 +1,19 @@
+class SceneMain extends Phaser.Scene{
+   constructor() {
+      super({key: 'SceneMain'});
+   }
+   preload(){
+      this.load.image('title','assets/title.png');
+   }
+   create(){
+      this.add.image(config.width/2, config.height/2, 'title');
+      this.time.addEvent({
+         delay: 3000,
+         callback: function(){
+            this.scene.start('SceneOne');
+         },
+         callbackScope: this,
+         repeat: this.robotTarget - 1
+      });
+   }
+}
